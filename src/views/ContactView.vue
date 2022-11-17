3896da65a7e07121add9fd14e2294b0a85d9fd5b
AhmadAlharbi <ahmaadzaid7@gmail.com>
2022-11-17 20:36:31 +0300

    ..

---
+++ src/views/ContactView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useFlash } from "@/composables/useFlash";
+let { flash } = useFlash();
+</script>
+<template>
+  <div class="contact">
+    <h1>This is an contact page</h1>
+    <button @click="flash('warning', 'Are you Sure?', 'warning')">Click</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .contact {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useFlash } from "@/composables/useFlash";
+import { ref, watch } from "vue";
+import { useStorage } from "@/composables/useStorage";
+let { flash } = useFlash();
+let food = useStorage("food", "tacos");
+</script>
+
+<template>
+  <main>
+    <p>What is your fav food?</p>
+    <input type="text" v-model="food" />
+    <p>How old are you></p>
+    <input type="text" v-model="age" />
+    <!-- <TheWelcome /> -->
+    <button @click="flash('Test', 'it works on the home page')">
+      Click me
+    </button>
+  </main>
+</template>
